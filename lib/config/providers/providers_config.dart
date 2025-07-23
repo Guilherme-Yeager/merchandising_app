69ef3c712b9ca63fce82f402434cfc97d1072e20
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2025-07-23 16:01:48 -0300

    feat(providers): adicionado os demais providers na lista do app.

--- lib/config/providers/providers_config.dart
+++ lib/config/providers/providers_config.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:merchandising_app/ui/auth/login/view_models/login_viewmodel.dart';
+import 'package:merchandising_app/ui/auth/logout/view_models/logout_viewmodel.dart';
+import 'package:merchandising_app/ui/splash/view_models/splash_viewmodel.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 abstract class ProvidersConfig {
   static List<SingleChildWidget> get providers => [
     ChangeNotifierProvider(create: (_) => LoginViewmodel()),
+    ChangeNotifierProvider(create: (_) => LogoutViewmodel()),
+    ChangeNotifierProvider(create: (_) => ChangeNotifier()),
+    ChangeNotifierProvider(create: (_) => SplashViewmodel()),
   ];
 }
