1435fcf6e1dd96cdfe2ab4b75ff892cba18d03af
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2025-07-24 11:51:20 -0300

    remove(navigator): remoção do app_navigator do core da ui.

--- lib/ui/core/navigator/app_navigator.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract class AppNavigator {
-  static void changePage(BuildContext context, String routeName) {
-    Navigator.popAndPushNamed(context, routeName);
-  }
-}
