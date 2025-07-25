d51c5dd69a1554463f1010b995f4025a3410c386
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2025-07-25 11:49:14 -0300

    fix(userModel): alterando campo matricula para codusur devido a alteração na busca no banco e utlização do textHelper para pegar o nome do promotor;

--- lib/domain/models/user/user_model.dart
+++ lib/domain/models/user/user_model.dart
@@ -1,12 +1,17 @@
+import 'package:merchandising_app/utils/helpers/text_helper.dart';
+
 class UserModel {
-  final int matricula;
+  final int codusur;
   final String nome;
 
-  UserModel({required this.matricula, required this.nome});
+  UserModel({required this.codusur, required this.nome});
 
-  Map<String, dynamic> toJson() => {"matricula": matricula, "nome": nome};
+  Map<String, dynamic> toJson() => {"codusur": codusur, "nome": nome};
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
-    return UserModel(matricula: json["matricula"], nome: json["nome"]);
+    return UserModel(
+      codusur: json["codusur"],
+      nome: TextHelper.extrairNome(json["nome"]),
+    );
   }
 }
