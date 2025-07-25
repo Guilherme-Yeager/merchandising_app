4510352ea9495c8a2fc1dbe8324eee7c78aab3cc
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2025-07-25 11:43:21 -0300

    feat(textHelper): implementado função para extrair uma string entre parênteses dado um texto utilizando Regex.

--- lib/utils/helpers/text_helper.dart
+++ lib/utils/helpers/text_helper.dart
@@ -1,3 +1,7 @@
-String primeiroNome(String nome) {
-  return nome.trim().split(' ').first;
+abstract class TextHelper {
+  static String extrairNome(String texto) {
+    RegExp exp = RegExp(r"\((.*?)\)");
+    RegExpMatch? match = exp.firstMatch(texto);
+    return match != null ? match.group(1) ?? "" : "";
+  }
 }
