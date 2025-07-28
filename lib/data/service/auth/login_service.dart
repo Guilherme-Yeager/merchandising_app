b5f96cf4416fc05ddd4a0e1c8424f78c8dec5d44
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2025-07-28 18:20:31 -0300

    docs(service): adiciona dumentação nas classes de serviço. #close 1

--- lib/data/service/auth/login_service.dart
+++ lib/data/service/auth/login_service.dart
@@ -1,6 +1,13 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+/// Serviço responsável por autenticar usuários utilizando o Supabase.
 class LoginService {
+  /// Realiza o login com e-mail e senha.
+  ///
+  /// - [email]: endereço de e-mail do usuário.
+  /// - [password]: senha associada ao e-mail.
+  ///
+  /// Retorna um [AuthResponse] com informações da sessão atual.
   Future<AuthResponse> logar(String email, String password) async {
     return await Supabase.instance.client.auth.signInWithPassword(
       email: email,
--- lib/data/service/user/user_service.dart
+++ lib/data/service/user/user_service.dart
@@ -1,6 +1,13 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+/// Serviço responsável por obter informações do usuário logado no supabase.
 class UserService {
+  /// Retorna informações do usuário correspondente ao [uuid].
+  ///
+  /// - [uuid]: identificador do usuário.
+  ///
+  /// Retorna um `Map<String, dynamic>` com os dados do usuário,
+  /// ou `null` caso nenhum usuário seja encontrado.
   Future<Map<String, dynamic>?> get(String uuid) async {
     return await Supabase.instance.client
         .from('pcusuari')
