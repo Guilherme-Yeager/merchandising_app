79448da2b9befa164332ded133b4385d65c55af8
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2025-08-04 10:47:50 -0300

    feat(pedioModel): propriedade do pedido definidos. Close #8.

--- lib/domain/models/pedido/pedido_model.dart
+++ lib/domain/models/pedido/pedido_model.dart
@@ -1,2 +1,37 @@
 /// Modelo que representa um pedido associado a um cliente.
-class PedidoModel {}
+class PedidoModel {
+  /// Código do produto.
+  final int codprod;
+
+  /// Descrição do produto.
+  final String descricao;
+
+  /// Quantidade disponível do produto
+  final double qtest;
+
+  /// Preço de venda do produto.
+  final double pvenda;
+
+  PedidoModel({
+    required this.codprod,
+    required this.descricao,
+    required this.qtest,
+    required this.pvenda,
+  });
+
+  Map<String, dynamic> toJson() => {
+    "codprod": codprod,
+    "descricao": descricao,
+    "qtest": qtest,
+    "pvenda": pvenda,
+  };
+
+  factory PedidoModel.fromJson(Map<String, dynamic> json) {
+    return PedidoModel(
+      codprod: json["codprod"],
+      descricao: json["descricao"],
+      qtest: json["qtest"],
+      pvenda: json["pvenda"],
+    );
+  }
+}
