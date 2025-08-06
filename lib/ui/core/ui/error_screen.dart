65282a5162fde514c7b9386523eaff66fe4c5b81
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2025-08-06 09:24:29 -0300

    add(errorScreen): adicionada tela de erro. #24

---
+++ lib/ui/core/ui/error_screen.dart
@@ -0,0 +1,79 @@
+/*
+ * Baseado em código de Abu Anwar, sob licença MIT.
+ * Copyright (c) 2021 Abu Anwar
+ *
+ * Modificações feitas por Guilherme em 2025.
+ */
+import 'package:flutter/material.dart';
+import 'package:merchandising_app/config/supabase/supabase_config.dart';
+import 'package:merchandising_app/ui/core/themes/app_colors.dart';
+
+class ErrorScreen extends StatelessWidget {
+  final String mensagem;
+
+  const ErrorScreen({super.key, required this.mensagem});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('assets/images/error.png', fit: BoxFit.cover),
+          Positioned(
+            bottom: MediaQuery.of(context).size.height * 0.14,
+            left: 0,
+            right: 0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                  child: Text(
+                    'Error: $mensagem =(',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 18),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: AppColors.buttonLogin,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 16,
+                    ),
+                  ),
+                  onPressed: () {
+                    SupabaseConfig.unsubscribeRealTimeChanges();
+                    Navigator.pushNamedAndRemoveUntil(
+                      context,
+                      '/login',
+                      (Route<dynamic> route) => false,
+                    );
+                  },
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
