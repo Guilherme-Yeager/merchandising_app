aeef9db1d4a49bf03f1ec179f1188466fa2572e4
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2025-08-08 17:26:51 -0300

    feat: adicionado fluxo para exceção durante o build.

--- Codigo/lib/main.dart
+++ Codigo/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:merchandising_app/config/providers/providers_config.dart';
 import 'package:merchandising_app/config/supabase/supabase_config.dart';
 import 'package:merchandising_app/routing/routes.dart';
+import 'package:merchandising_app/ui/core/ui/error_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
@@ -13,6 +14,9 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    return ErrorScreen(mensagem: details.exceptionAsString());
+  };
   await SupabaseConfig.initialize();
   runApp(
     DevicePreview(
