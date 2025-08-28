9ddba139908b24f26fb833a1bbe5619f83cda7a6
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2025-08-28 14:23:11 -0300

    test(homeViewModel): separação dos casos para o subtítulo do AppBar,

--- Codigo/test/ui/home/view_models/home_viewmodel_test.dart
+++ Codigo/test/ui/home/view_models/home_viewmodel_test.dart
@@ -3,20 +3,33 @@ import 'package:merchandising_app/ui/home/view_models/home_viewmodel.dart';
 
 void main() {
   group('HomeViewModel', () {
-    test('Atribuição do título da AppBar', () async {
-      final HomeViewModel homeViewModel = HomeViewModel();
+    late HomeViewModel homeViewModel;
 
+    setUp(() {
+      homeViewModel = HomeViewModel();
+    });
+
+    test('Atribuição do título da AppBar', () async {
+      // Act
       homeViewModel.updateTitleAppBar("Início");
+
+      // Assert
       expect(homeViewModel.titleAppBar, "Início");
     });
 
-    test('Atribuição do subtítulo da AppBar', () async {
-      final HomeViewModel homeViewModel = HomeViewModel();
-
+    test('Atribuição do subtítulo da AppBar igual a null', () async {
+      // Act
       homeViewModel.updateSubtitleAppBar(null);
-      expect(homeViewModel.subtitleAppBar, null);
 
+      // Assert
+      expect(homeViewModel.subtitleAppBar, null);
+    });
+
+    test('Atribuição do subtítulo da AppBar', () async {
+      // Act
       homeViewModel.updateSubtitleAppBar("Total: 6");
+
+      // Assert
       expect(homeViewModel.subtitleAppBar, "Total: 6");
     });
   });
